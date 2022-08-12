bbd38b676fb8583e60901732f670c24d2dd93f1a
Marquesjob <patrickmarques.job@hotmail.com>
2022-08-12 20:26:57 -0300

    - first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuccessComponent } from './components/success/success.component';
+import { FailComponent } from './components/fail/fail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessComponent,
+    FailComponent,
   ],
   imports: [
     BrowserModule,
